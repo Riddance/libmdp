d48278846b60f00bdd7f54feca02f4311bdecbc9
caiyiheng <cyh@ejoy.com>
2016-11-07 18:35:19 +0800

    up ui

--- ci_build.sh
+++ ci_build.sh
@@ -3,14 +3,16 @@
 set -x
 set -e
 
-git clone https://github.com/zeromq/libzmq
-(
-    cd libzmq
-    mkdir cmake-build
-    cd cmake-build
-    cmake ..
-    make -j 4
-)
+if [ ! -d "libzmq" ]; then
+    git clone https://github.com/zeromq/libzmq
+    (
+        cd libzmq
+        mkdir cmake-build
+        cd cmake-build
+        cmake ..
+        make -j 4
+    )
+fi
 
 mkdir build
 cd build
