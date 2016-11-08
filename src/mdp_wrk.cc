fe85ee3b7e28a7ed9d4fb88e91e9a2f03766daca
Riddance <xiongyi@XiongdeMacBook-Air.local>
2016-11-08 17:58:00 +0800

    fix bugs
    fix bugs

--- src/mdp_wrk.cc
+++ src/mdp_wrk.cc
@@ -59,10 +59,11 @@ int WrkApi::Send(std::string& msg, std::string& to)
     MDP_ASSERT(!to.empty());
 
     mdp::MdpMsg mdp_message;
+    mdp_message.PushFront(msg);
     mdp_message.PushFront("");
     mdp_message.PushFront(to);
 
-    return RealSend(MDPW_REPORT, "", msg);
+    return RealSend(MDPW_REPORT, "", mdp_message);
 }
 
 int WrkApi::SetSockOpt(int option, const void *optval, size_t optvallen)
@@ -85,18 +86,17 @@ int WrkApi::Connect()
 
     MDP_ASSERT(rc == 0);
     m_socket = zmq_socket(m_ctx, ZMQ_DEALER);
-    if (socket_ == NULL)
+    if (m_socket == NULL)
         return -1;
 
     rc = zmq_connect(m_socket, m_broker.c_str());
     if (rc != 0)
         return rc;
 
-    m_heartbeat = mdp::mdp_time() + m_heartbeat;
+    m_heartbeat_at = mdp::mdp_time() + HEARTBEAT_INTERVAL;
 
     mdp::MdpMsg mdp_message;
-    mdp_message.PushFront();
-
+    //mdp_message.PushFront();
     return RealSend((char*)MDPW_READY, m_service, mdp_message);
 }
 
@@ -126,7 +126,7 @@ void WrkApi::Close()
 int WrkApi::ProcessRecvCmd(mdp::MdpMsg& mdp_message, std::string& message, std::string& from)
 {
     int rc = 0;
-    MDP_ASSERT(mdp_message.size() >= 3);
+    MDP_ASSERT(mdp_message.Size() >= 3);
     std::string empty   = mdp_message.PopFront();
     std::string header  = mdp_message.PopFront();
     MDP_ASSERT(header == std::string (MDPW_WORKER));
@@ -134,7 +134,7 @@ int WrkApi::ProcessRecvCmd(mdp::MdpMsg& mdp_message, std::string& message, std::
 
     if (cmd == std::string(MDPW_REQUEST))
     {
-        MDP_ASSERT(mdp_message.size() == 3);
+        MDP_ASSERT(mdp_message.Size() == 3);
         from    = mdp_message.PopFront();
         empty   = mdp_message.PopFront();
         message = mdp_message.PopFront();
