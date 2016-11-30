13800d1f556a41fa17aaa38867d8a70742c51472
caiyiheng <cyh@ejoy.com>
2016-11-30 10:26:47 +0800

    update build ci

--- ci_build.sh
+++ ci_build.sh
@@ -3,16 +3,13 @@
 set -x
 set -e
 
-git submodule update --init --recursive
-(
-    cd libzmq
-    mkdir cmake-build
-    cd cmake-build
-    cmake ..
-    sudo make install
-    sudo ldconfig
-    cd ..
-)
+cd libzmq
+mkdir cmake-build
+cd cmake-build
+cmake ..
+sudo make install
+sudo ldconfig
+cd ..
 
 mkdir build
 cd build
